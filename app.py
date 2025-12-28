b94c3f0ae5d3b160ca9213d480cee743ec1443ea
Anas215409 <akahmadanas873@gmail.com>
2025-12-28 14:59:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,8 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
+
+
 st.title('🏠House Price prediction using ML')
 
+st.image('https://media.tenor.com/B9G3PV_TOBYAAAAM/happy-birthday.gif')
